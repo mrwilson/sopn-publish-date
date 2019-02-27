95cb3e7a07581c5cca27b933b5cb5d2330b55126
Alex Wilson <alex+github@probablyfine.co.uk>
2019-02-27 12:34:33 +0000

    feat(implementation): Throw exception for ambiguous ids
    * Create internal function that takes (country, poll date) for unambiguous election types such as nia, nwa, sp, and gla.
    * Throw exception for ambiguous types such as local, parl

--- sopn_publish_date/__init__.py
+++ sopn_publish_date/__init__.py
@@ -1,19 +1,32 @@
-from sopn_publish_date.calendars import working_days, ScottishHolidays
+from sopn_publish_date.calendars import working_days, ScottishHolidays, EnglandAndWalesHolidays, NorthernIrelandHolidays
 from datetime import datetime
 
 
-def sopn_publish_date(election_id):
+def sopn_publish_date_for_id(election_id):
 
     election_type, *_, poll_date = election_id.split(".")
 
-    return _sopn_publish_date(election_type, datetime.strptime(poll_date, "%Y-%m-%d"))
+    date_of_poll = datetime.strptime(poll_date, "%Y-%m-%d")
+
+    if election_type == 'nia':
+        return sopn_publish_date('northern-ireland', date_of_poll)
+    elif election_type == 'sp':
+        return sopn_publish_date('scotland', date_of_poll)
+    elif election_type == 'naw':
+        return sopn_publish_date('wales', date_of_poll)
+    elif election_type == 'gla':
+        return date_of_poll - working_days(23)
+    else:
+        raise Exception("Cannot derive country from ambiguous election id [%s]" % election_id)
 
 
-def _sopn_publish_date(election_type, poll_date):
+def sopn_publish_date(country, poll_date):
 
-    if election_type in ["local", "parl"]:
-        return poll_date - working_days(19)
-    elif election_type == "sp":
+    if country == 'northern-ireland':
+        return poll_date - working_days(19, calendar=NorthernIrelandHolidays())
+    elif country == 'scotland':
         return poll_date - working_days(23, calendar=ScottishHolidays())
-
-    raise Exception("Not implemented for election type: %s" % election_type)
+    elif country == 'england' or country == 'wales':
+        return poll_date - working_days(19, calendar=EnglandAndWalesHolidays())
+    else:
+        raise Exception("Not implemented for election: [%s,%d]" % (country, poll_date))
--- sopn_publish_date/calendars.py
+++ sopn_publish_date/calendars.py
@@ -51,5 +51,27 @@ class EnglandAndWalesHolidays(AbstractHolidayCalendar):
     ]
 
 
+class NorthernIrelandHolidays(AbstractHolidayCalendar):
+    rules = [
+        Holiday("New Years Day", month=1, day=1, observance=next_monday),
+        Holiday("St Patrick's Day", month=3, day=17, observance=next_monday),
+        GoodFriday,
+        EasterMonday,
+        Holiday(
+            "Early May bank holiday", month=5, day=1, offset=DateOffset(weekday=MO(1))
+        ),
+        Holiday(
+            "Spring bank holiday", month=5, day=31, offset=DateOffset(weekday=MO(-1))
+        ),
+        Holiday(
+            "Battle of the Boyne / Orangemen's Day", month=7, day=12, offset=next_monday),
+        Holiday(
+            "Summer bank holiday", month=8, day=31, offset=DateOffset(weekday=MO(-1))
+        ),
+        Holiday("Christmas Day", month=12, day=25, observance=next_monday),
+        Holiday("Boxing Day", month=12, day=26, observance=next_monday_or_tuesday),
+    ]
+
+
 def working_days(count, calendar=EnglandAndWalesHolidays()):
     return CDay(count, calendar=calendar)
