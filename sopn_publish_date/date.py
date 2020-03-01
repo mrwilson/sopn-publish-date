b218ce82210a174496e9d42aa494191e54ba652b
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:23:01 +0000

    feat(date): Ignore weekends in calculation

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -2,4 +2,10 @@ from datetime import date, timedelta
 
 
 def days_before(poll_date: date, days: int) -> date:
-    return poll_date - timedelta(days=days)
+    while days > 0:
+        poll_date -= timedelta(days=1)
+
+        if poll_date.weekday() not in [5, 6]:
+            days -= 1
+
+    return poll_date
