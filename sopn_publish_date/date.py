fd38f85594bbd8744f88a3d480d462ba80ff4cdf
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:27:54 +0000

    refactor(date): Support a list of exempted dates during calculation

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -1,14 +1,18 @@
+from typing import List
 from datetime import date, timedelta
 
 SATURDAY = 5
 SUNDAY = 6
 
 
-def days_before(poll_date: date, days: int) -> date:
+def days_before(poll_date: date, days: int, exempted_days: List[date] = []) -> date:
     while days > 0:
         poll_date -= timedelta(days=1)
 
-        if poll_date.weekday() not in [SATURDAY, SUNDAY]:
+        if (
+            poll_date.weekday() not in [SATURDAY, SUNDAY]
+            and poll_date not in exempted_days
+        ):
             days -= 1
 
     return poll_date
