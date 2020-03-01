4a97d3dc4aaba07d65a5c50bd852cba1691b6baf
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:28:57 +0000

    refactor(date): Split double conditional into two break statements

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -9,10 +9,12 @@ def days_before(poll_date: date, days: int, exempted_days: List[date] = []) -> d
     while days > 0:
         poll_date -= timedelta(days=1)
 
-        if (
-            poll_date.weekday() not in [SATURDAY, SUNDAY]
-            and poll_date not in exempted_days
-        ):
-            days -= 1
+        if poll_date.weekday() in [SATURDAY, SUNDAY]:
+            continue
+
+        if poll_date in exempted_days:
+            continue
+
+        days -= 1
 
     return poll_date
