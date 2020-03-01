e2f831023c7c3cfdf1dba63ab2f278280d25540b
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 17:09:05 +0000

    feat(date): Introduce DateMatcher concept for ignored days

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -5,14 +5,33 @@ SATURDAY = 5
 SUNDAY = 6
 
 
-def days_before(poll_date: date, days: int, exempted_days: List[date] = []) -> date:
+class DateMatcher:
+    def __init__(self, day, month, year=None):
+        self.month = month
+        self.day = day
+        self.year = year
+
+    def matches(self, other: date):
+        if self.day != other.day:
+            return False
+
+        if self.month != other.month:
+            return False
+
+        if self.year is not None and self.year != other.year:
+            return False
+
+        return True
+
+
+def days_before(poll_date: date, days: int, ignore: List[DateMatcher] = []) -> date:
     while days > 0:
         poll_date -= timedelta(days=1)
 
         if poll_date.weekday() in [SATURDAY, SUNDAY]:
             continue
 
-        if poll_date in exempted_days:
+        if any([day.matches(poll_date) for day in ignore]):
             continue
 
         days -= 1
