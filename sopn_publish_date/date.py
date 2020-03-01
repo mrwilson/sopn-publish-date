30eac91bd3ffc2b23d4702067063e42c08bb9879
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 16:24:16 +0000

    refactor(date): Name indexes for weekend days

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -1,11 +1,14 @@
 from datetime import date, timedelta
 
+SATURDAY = 5
+SUNDAY = 6
+
 
 def days_before(poll_date: date, days: int) -> date:
     while days > 0:
         poll_date -= timedelta(days=1)
 
-        if poll_date.weekday() not in [5, 6]:
+        if poll_date.weekday() not in [SATURDAY, SUNDAY]:
             days -= 1
 
     return poll_date
