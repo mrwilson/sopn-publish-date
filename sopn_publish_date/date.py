c5ab320302fb4922b231c93cd9e8140920971cdf
Alex Wilson <gpg@probablyfine.co.uk>
2020-03-01 18:28:02 +0000

    feat(date): Add documentation for new date class

--- sopn_publish_date/date.py
+++ sopn_publish_date/date.py
@@ -6,13 +6,23 @@ SUNDAY = 6
 
 
 class DateMatcher:
-    def __init__(self, day, month, year=None, name=None):
+    """
+    An object that represents a matcher against datetime.date objects, given a day, a month, and an optional year.
+    """
+
+    def __init__(self, day: int, month: int, year: int = None, name: str = None):
         self.name = name
         self.month = month
         self.day = day
         self.year = year
 
-    def matches(self, other: date):
+    def matches(self, other: date) -> bool:
+        """
+        Return whether the input date matches the attributes of this class
+
+        :param other: the date being matched against
+        :return a boolean representing whether the input matched against this class's attributes
+        """
         if self.day != other.day:
             return False
 
@@ -25,7 +35,16 @@ class DateMatcher:
         return True
 
 
-def days_before(poll_date: date, days: int, ignore: List[DateMatcher] = []) -> date:
+def days_before(poll_date: date, days: int, ignore: List[DateMatcher] = None) -> date:
+    """
+    Return date corresponding to `days` working days before `poll_date`, not counting the list of provided exemptions
+
+    :param poll_date: the date of the poll
+    :param days: the number of days before the poll date
+    :param ignore: the list of DateMatchers to ignore in the look-back calculation
+    :return: the calculated date
+    """
+
     while days > 0:
         poll_date -= timedelta(days=1)
 
